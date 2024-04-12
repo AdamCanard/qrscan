4674daf2341a9c6a0468c1a5caab4b5f467311c7
AdamCanard <azcunard@gmail.com>
2024-04-12 15:34:34 -0600

    fix?

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import mkcert from 'vite-plugin-mkcert'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: { 
-    https: true, 
-    host: true 
+  server: {
+    https: true,
+    host: true,
   },
   plugins: [react(), mkcert()],
-})
+  base: "/qrscan/",
+});
